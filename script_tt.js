2972840c6c7e3611f7fa4f93859455c6f433ebc8
Alex Gregori Tomás <reproteq@gmail.com>
2019-05-09 16:37:57 +0200

    Update script_tt.js

--- script_tt.js
+++ script_tt.js
@@ -17,43 +17,4 @@ var div = document.createElement('div');
 	var graph = ui.editor.graph;
 //
 
-////Sidebar libs in
-
-////Sidebar libs out
-	
-////Custom Menu in    
-//    // Adds resource for action
-    mxResources.parse('pyroAction1=PyroAction1');
-		mxResources.parse('pyroAction2=PyroAction2');
-		mxResources.parse('pyroAction3=PyroAction3');
-//
-//    // Adds action
-  ui.actions.addAction('pyroAction1', function() {
-       mxUtils.alert('Pyro Menu vacio1');
-   });
-	
-	  ui.actions.addAction('pyroAction2', function() {
-       mxUtils.alert('Pyro Menu vacio2');
-   });
-		
-		  ui.actions.addAction('pyroAction3', function() {
-       mxUtils.alert('Pyro Menu vacio3');
-   });
-//
-//    // Adds menu
-    ui.menubar.addMenu('PyroMenu', function(menu, parent) {
-       ui.menus.addMenuItem(menu, 'pyroAction1');
-			 ui.menus.addMenuItem(menu, 'pyroAction2');
-			 ui.menus.addMenuItem(menu, 'pyroAction3');
-			 
-   });
-//
-//    // Reorders menubar
-    ui.menubar.container.insertBefore(ui.menubar.container.lastChild,
-    ui.menubar.container.lastChild.previousSibling.previousSibling);
-//
- 
-////Custom Menu 
-
- 
 });
